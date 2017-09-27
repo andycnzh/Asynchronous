125cca2cf140ad14e15f3b7307157f8d1579ff98
andycnzh <andycnzh@outlook.com>
2017-09-27 17:54:44 +0800

    Add blocked code

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string[] domains = new string[3] { "www.google.com", "www.bing.com", "www.baidu.com" };
+            for (int i = 0; i < domains.Length; i++)
+            {
+                AddAFavicon(domains[i]);
+            }
+        }
+
+        private void AddAFavicon(string domain)
+        {
+            WebClient webClient = new WebClient();
+            byte[] bytes = webClient.DownloadData("http://" + domain + "/favicon.ico");
+            Image imageControl = MakeImageControl(bytes);
+            //WrapPanel1.Children.Add(imageControl);
+        }
+
+        private Image MakeImageControl(byte[] bytes)
+        {
+            //throw new NotImplementedException();
+            return null;
+        }
+    }
+}
