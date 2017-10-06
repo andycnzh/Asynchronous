a4d48e89f060da7936268a846928414ea9271197
andycnzh <andycnzh@outlook.com>
2017-10-06 21:04:21 +0800

    Add unit test

---
+++ FaviconBrowser.UnitTests/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace FaviconBrowser.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            TaskTest t = new TaskTest();
+
+            var result = await t.TaskMethod();
+
+            Assert.AreEqual(result, 3);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
---
+++ FaviconBrowser/TaskTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaviconBrowser
+{
+    public class TaskTest
+    {
+        public async Task<int> TaskMethod()
+        {
+            await Task.Delay(3000);
+
+            return 3;
+        }
+    }
+}
